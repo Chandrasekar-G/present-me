ecbbe9be61fdc01e88302ae4efdecf7d1a470d20
Chandrasekar Gokulanathan <g.chandrasekar1993@gmail.com>
2017-10-26 18:17:05 +0530

    Added Travis and changed Test scripts

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('present-me App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Present Me!!');
   });
 });
