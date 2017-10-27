c9bfe971c483a53143ded9ebc5d72ff56cf04a8d
Chandrasekar Gokulanathan <g.chandrasekar1993@gmail.com>
2017-10-27 17:43:37 +0530

    Generated titles and abstracts from dataset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+
+import  data  from './data/data-set';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+  dataSet = [];
+  currentTalk = {};
+
+  ngOnInit() {
+    this.dataSet = data;
+    this.generateTalk();
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if(event.code == "Space") {
+      this.generateTalk();
+    }
+  }
+
+  generateTalk() {
+    this.currentTalk = this.dataSet[Math.floor(Math.random()*this.dataSet.length)];
+  }
+
 }
